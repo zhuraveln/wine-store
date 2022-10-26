eda906e671c9cfd2d2a23324120fa6b9f217bb5e
Zhuraveln <nikitazuravel@Nikitas-MacBook-Pro.local>
2022-10-26 16:10:11 +0300

    Add adaptive layout for all pages

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -66,17 +66,15 @@ const Cart: React.FC = () => {
             <svg viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg">
               <path d="M39.3756,48.0022l30.47-25.39a6.0035,6.0035,0,0,0-7.6878-9.223L26.1563,43.3906a6.0092,6.0092,0,0,0,0,9.2231L62.1578,82.615a6.0035,6.0035,0,0,0,7.6878-9.2231Z" />
             </svg>
-            <span>Вернуться назад</span>
+            <span>Вернуться в магазин</span>
           </Link>
-          <Link to="/cart/confirm" onClick={clearAll}>
-            <div className={styles.buttonPay}>
-              <span>Оплатить сейчас</span>
-            </div>
+          <Link to="/cart/confirm" onClick={clearAll} className={styles.buttonPay}>
+            <span>Оплатить сейчас</span>
           </Link>
         </div>
       </div>
       <Modal
-        text={'Вы точно хотите отчистить корзину?'}
+        text={'Вы точно хотите очистить корзину?'}
         visible={modalVisible}
         setVisible={modalSetVisible}
       />
